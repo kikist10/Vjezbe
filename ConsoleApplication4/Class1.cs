3a7bfc8516ce9ec82c9ca6915846cdba0eee765e
Maja Štula <kiki@fesb.hr>
2018-03-16 10:51:55 +0100

    Update Class1.cs

--- ConsoleApplication4/Class1.cs
+++ ConsoleApplication4/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace ConsoleApplication4
 {
     class Class1
     {
+        public void aaa()
+        {}
     }
 }
